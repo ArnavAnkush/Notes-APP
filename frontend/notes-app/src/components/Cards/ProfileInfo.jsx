a7b7b4c9b6de9a6a481ab5dbfc99a7e26021ad3c
ArnavAnkush <arnav.ankush.personal@gmail.com>
2026-02-14 23:35:17 +0530

    Navbar Profile Card

---
+++ frontend/notes-app/src/components/Cards/ProfileInfo.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./ProfileInfo.scss";
+import { getInitials } from "../../utils/helper";
+
+const ProfileInfo = ({ onLogout }) => {
+  return (
+    <div className="profile-container">
+      <div className="profile-pic">{getInitials("Arnav Ankush")}</div>
+
+      <div>
+        <p>Arnav Ankush</p>
+        <button className="" onClick={onLogout}>
+          LogOut
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileInfo;
--- frontend/notes-app/src/components/Navbar/Navbar.jsx
+++ frontend/notes-app/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import "./Navbar.scss";
+import ProfileInfo from "../Cards/ProfileInfo";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate;
+
+  const onLogout = () => {
+    navigate("/login");
+  };
   return (
     <div className="nav-container">
       <h2>Notes</h2>
+
+      <ProfileInfo onLogout={onLogout} />
     </div>
   );
 };
